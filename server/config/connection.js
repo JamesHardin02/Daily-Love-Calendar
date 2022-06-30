ac6a3b60a55dd842e8389f15488d83b2c91be472
vidurham <vdurham@prideseals.com>
2022-06-29 20:49:34 -0400

    setup for Heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/daily-love-calendar', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/daily-love-calendar', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
--- server/models/Calendar.js
+++ server/models/Calendar.js
@@ -2,7 +2,16 @@ const { Schema, model } = require('mongoose');
 
 const CalendarSchema = new Schema(
     {
+        name: {
+            type: String,
+            required: true
+        },
+        pictures: {
 
+        },
+        quotes: {
+            
+        }
     }
 )
 
